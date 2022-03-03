8669f7de5742d954c4b5ef74ea34b28662f5b138
Mikkel RINGAUD <vexitofficial@gmail.com>
2022-03-03 19:23:40 +0100

    Add front-end part using Vite+React

---
+++ website/src/main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// TailwindCSS
+import "./styles/globals.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>Rikka</h1>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
